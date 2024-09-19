d38d05a2c0b29d2ac4ab6d23709cd2f9d3c8d106
Jannik Brozy <jannik.brozy@student.fh-kiel.de>
2024-09-19 12:30:38 +0200

    init

---
+++ fetch.sh
@@ -0,0 +1,1 @@
+wget https://db.iue.fh-kiel.de/ndbk/scenarios/ --no-host-directories --recursive --cut-dirs=100 --no-parent --level=5 --accept "*uber*.csv" -P ./data
---
+++ start.sh
@@ -0,0 +1,1 @@
+./fetch.sh && docker-compose up -d
